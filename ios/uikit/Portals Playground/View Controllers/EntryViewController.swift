b332ab56a084c56b86787ba3351713c0c3cab170
Eric Horodyski <horodyski@ionic.io>
2022-12-14 13:36:26 -0500

    ios(ui-kit): add Portals Key

---
+++ ios/uikit/Portals Playground/View Controllers/EntryViewController.swift
@@ -0,0 +1,29 @@
+//
+//  EntryViewController.swift
+//  Portals Playground
+//
+//  Created by Eric Horodyski on 12/14/22.
+//
+
+import UIKit
+
+class EntryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Portals Playground"
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ios/uikit/Portals Playground/View Controllers/PortalsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  PortalsViewController.swift
+//  Portals Playground
+//
+//  Created by Eric Horodyski on 12/14/22.
+//
+
+import UIKit
+
+class PortalsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Portals"
+        // Do any additional setup after loading the view.
+    }
+}
